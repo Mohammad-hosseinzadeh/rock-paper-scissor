02098917be5ad7a3f08622a66da2fbf80516480f
Mohammad-hosseinzadeh <webhosseinzadeh@gmail.com>
2022-09-15 14:48:27 +0430

    change scissor variable name

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let userScore = 0;
 let compScore = 0;
 const rock_div = document.querySelector("#r");
 const paper_div = document.querySelector("#p");
-const scissors_div = document.querySelector("#s");
+const scissor_div = document.querySelector("#s");
 const userscore_span = document.querySelector("#user-score");
 const compscore_span = document.querySelector("#comp-score");
 const result_p = document.querySelector(".result p");
@@ -17,7 +17,7 @@ function eventListeners() {
     paper_div.addEventListener("click", () => {
         game("p");
     });
-    scissors_div.addEventListener("click", () => {
+    scissor_div.addEventListener("click", () => {
         game("s");
     });
 }
