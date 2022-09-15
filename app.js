23ac1b7439a06ffc312087f1b4c2238483448547
Mohammad-hosseinzadeh <webhosseinzadeh@gmail.com>
2022-09-15 12:13:23 +0430

    init project files

---
+++ app.js
@@ -0,0 +1,10 @@
+// variabels
+let userScore = 0;
+let compScore = 0;
+const rock_div = document.querySelector("#r");
+const paper_div = document.querySelector("#p");
+const scissors_div = document.querySelector("#s");
+const userscore_span = document.querySelector("#user-score");
+const compscore_span = document.querySelector("#comp-score");
+const result_p = document.querySelector(".result p");
+
